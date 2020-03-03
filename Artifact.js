1b4e932e4e04333129c52016ccdaf07c99c4911e
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-03-02 21:30:51 -0800

    gone a bit overboard and ran the html file

--- Artifact.js
+++ Artifact.js
@@ -1,2 +1,11 @@
 //Hanson Nguyen
 //completed not yet
+
+let artifactArray = [10];
+exports.artifactArray = artifactArray; 
+exports.set = function(value) {
+    artifactArray.push(value);
+};
+exports.get = function() {
+    return artifactArray;
+};
