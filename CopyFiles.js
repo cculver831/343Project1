d3e4e1e56576d8faf0d70ad53196725beac16ddd
Joseph-Burgos <josephburgos562@gmail.com>
2020-03-06 13:40:58 -0800

    CopyFIles Updated, Temp files were created

--- CopyFiles.js
+++ CopyFiles.js
@@ -3,10 +3,10 @@
 const path = require('path')
 const fs = require('fs')
 let ReadFiles = require('./ReadFiles');
-ReadFiles.ReturnArray
+ReadFiles.ArrayResult
 
 
-var len = ReadFiles.ArrayResult.length - 1
+var len = ReadFiles.ArrayResult.length
 //testing
 console.log("Before loop")
 console.log(ReadFiles.ArrayResult.toString)
@@ -20,26 +20,26 @@ fs.appendFile("Manifest.txt", '', function (err) {
 
 //goes through array of file paths and copies into temp
 for(var i = 0; i < len; i++){
-    console.log(String(ReadFiles.ReturnArray[i]))
+    console.log(String(ReadFiles.ArrayResult[i]))
     //copy file to folder
-    fs.copyFile(ReadFiles.ReturnArray[i], path.join(path.dirname(__filename) + '\\' + 'MyApp' + '\\' + ".Repository" + "Temp" + "\\" + path.basename(ReadFiles.ReturnArray[i])), (err) => {
+    fs.copyFile(ReadFiles.ArrayResult[i], path.join(path.dirname(__filename) + '\\' + 'MyApp' + '\\' + ".Repository" + "\\"  + "Temp" + "\\" + path.basename(ReadFiles.ArrayResult[i])), (err) => {
         if (err) throw err;
         console.log('File was copied to destination');
       });
       
     //append information into the manifest
-    var fileInformation = "CLP NAME" + "=" + ReadFiles.ReturnArray[i];
+    var fileInformation = "CLP NAME" + "=" + ReadFiles.ArrayResult[i] + "\n";
     //appends info into files (file destination, content, error)
     fs.appendFile("Manifest.txt", fileInformation, function (err) {
         if (err) throw err;
-        console.log('Saved!');
+        console.log('Manifest Updated!');
       });
 }
 //append Date and time to manifest
 var d = new Date();
 fs.appendFile("Manifest.txt", d, function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Manifest Updated!');
   });
 
 //create and append command line on top of manifest.txt
