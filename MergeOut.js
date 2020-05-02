4a16121731444373f8f3fa08b9f99f54337f1051
stevenone11 <stevencenteno13@gmail.com>
2020-05-01 20:53:28 -0700

    Update MergeOut.js

--- MergeOut.js
+++ MergeOut.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 
-let ArraySourceLen; // will be the length of the manifest array
+let ArraySourceLen = 0; // will be the length of the manifest array
 
 
 
@@ -18,7 +18,7 @@ let ArraySourceLen; // will be the length of the manifest array
 //branchedRepoLoc is your version of the project that you may have branced out of
 //note that all these are directories not the actal file name
 function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
-    
+
     //gets the files from the manifest file and saves into an array
     //[[file path,artifact_id, manifest_path],[],[],[],[],[],[],[]] this array is the files of the manifest file
     let SourceFiles = getfilesManifest(repoLoc, R_ManifestLoc);
@@ -44,11 +44,12 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
 
         //start searching through the search folders starting from the length of the repository because they theoretically should
         //have the same amount of elements before new ones in the directory 
-        let startingPositionFileSearch = BranchDirFolders.length(); //will be the folder location of the repository
-        let fileSearchLen = fileSearchFolders.length(); //will be folder of repo + any other possible new folders to add to the repo
+        let startingPositionFileSearch = BranchDirFolders.length; //will be the folder location of the repository
+        let fileSearchLen = fileSearchFolders.length; //will be folder of repo + any other possible new folders to add to the repo
 
         //see if the files should be in the repo or in sub folders of the repo
         //if the folder does not exist, create it
+
         if(fileSearchLen != startingPositionFileSearch){
 
             let subfolder = T_BrancedRepoLoc; //save the current subfolder path
@@ -96,7 +97,7 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
                 //rename target file (old) 
                 //next 3 lines update to have "_MT" to old file
                 let suffix_old = path.extname(filePathSearch);
-                let updated_old = filePathSearch.substring(0, (str.length - suffix_old.length));
+                let updated_old = filePathSearch.substring(0, (filePathSearch.length - suffix_old.length));
                 updated_old = updated_old + '_MT' + suffix_old;
                 //replaces the name of the file
                 fs.rename(filePathSearch, updated_old, function (err) {
@@ -117,7 +118,7 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
                 //rename target file (old) 
                 //next 3 lines update to have "_MR" to old file
                 let suffix_new = path.extname(filePathSearch); 
-                let updated_new = filePathSearch.substring(0, (str.length - suffix_new.length));
+                let updated_new = filePathSearch.substring(0, (filePathSearch.length - suffix_new.length));
                 updated_new = updated_new + '_MR' + suffix_new;
                 //replaces the name of the file
                 fs.rename(filePathSearch, updated_new, function (err) {
@@ -128,32 +129,31 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
                 //add the new file path to the manifest array
                 manifestFiles.push(updated_new);
 
-
                 //copy grandma file(with extension _MG) into target location as well===============================
                 //REMEMBER TO ADD EVERY FILE THAT WE COPIED OVER TO AN ARRAY SO WE CAN CREATE A MANIFEST OF SAID FILES
 
-                let GrandmaManifest;
-                let GrandmaFile;
+                // let GrandmaManifest;
+                // let GrandmaFile;
 
-                //copy file form R to T because we renamed the old one
-                fs.copyFile(GrandmaFile, filePathSearch, (err) => {
-                //throws error if could not copy file to destination  
-                if (err) throw err;
-                });
+                // //copy file form R to T because we renamed the old one
+                // fs.copyFile(GrandmaFile, filePathSearch, (err) => {
+                // //throws error if could not copy file to destination  
+                // if (err) throw err;
+                // });
                 
-                //rename target file (old) 
-                //next 3 lines update to have "_MR" to old file
-                let suffix_Grandma = path.extname(filePathSearch);
-                let updated_Grandma = filePathSearch.substring(0, (str.length - suffix_Grandma.length));
-                updated_Grandma = updated_Grandma + '_MG' + suffix_Grandma;
-                //replaces the name of the file
-                fs.rename(filePathSearch, updated_Grandma, function (err) {
-                    if (err) throw err;
-                    console.log('File Renamed.');
-                });
+                // //rename target file (old) 
+                // //next 3 lines update to have "_MR" to old file
+                // let suffix_Grandma = path.extname(filePathSearch);
+                // let updated_Grandma = filePathSearch.substring(0, (str.length - suffix_Grandma.length));
+                // updated_Grandma = updated_Grandma + '_MG' + suffix_Grandma;
+                // //replaces the name of the file
+                // fs.rename(filePathSearch, updated_Grandma, function (err) {
+                //     if (err) throw err;
+                //     console.log('File Renamed.');
+                // });
 
-                //add the new file path to the manifest array
-                manifestFiles.push(updated_Grandma);
+                // //add the new file path to the manifest array
+                // manifestFiles.push(updated_Grandma);
             }
             
         }
@@ -161,6 +161,7 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
 
         //then if it doesnt exist, copy it over
         else{
+
             //copy files form R to T
             fs.copyFile(manifestIDPath, filePathSearch, (err) => {
             
@@ -174,7 +175,7 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
     }
 
     //create Manifest
-    let manifestLocation = createManifestFile(T_BrancedRepoLoc);
+    let manifestLocation = createManifestFile(repoLoc);
     
     //copy the files into the manifestwith their artifact ID
     for (let i = 0; i < manifestFiles.length; i++){
@@ -184,7 +185,7 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
         let artifact = require('./ArtifactRunner')(String(filepath));
         let fileAtrifact = artifact.getArtifact;
 
-        let manifestLine = filepath + "=" + fileAtrifact;
+        let manifestLine = fileAtrifact + "=" + filepath;
 
         fs.appendFile(manifestLocation, manifestLine +  "\n", function (err) {
         if (err) throw err;
@@ -210,14 +211,14 @@ function MergeOut(repoLoc, T_BrancedRepoLoc, R_ManifestLoc, command){
 
 //creates a mainifest file in the repository
 //returns the location of the manifest file
-function createManifestFile(T_BrancedRepoLoc){
+function createManifestFile(repoLoc){
 
     //reads the latest manifest file in the repository
-    let RepDirFiles = require('./ReadFiles')(String(T_BrancedRepoLoc));
+    let RepDirFiles = require('./ReadFiles')(String(repoLoc));
     let RepLatestMani = RepDirFiles.latestManiFile;
 
     //creates a new manifest file in the repository
-    let location = path.join(String(T_BrancedRepoLoc) + "\\"  + ".Temp" + "\\" + ".man" + String(Number(RepLatestMani) + 1) + ".rc" )
+    let location = path.join(String(repoLoc) + "\\"  + ".Temp" + "\\" + ".man" + String(Number(RepLatestMani) + 1) + ".rc" )
     fs.appendFile(location, "Commit " + ((Number(RepLatestMani)) + 1)  + ".source:\n", function (err) {
 
         //throws error if could not append file  
@@ -243,28 +244,29 @@ function getfilesManifest(repoDir, manifestDir){
     //goes through all files in the manifest and both gets their path and their check sums
     const data = fs.readFileSync(repoDir + "\\.Temp\\" + manifestDir, 'UTF-8'); //gets all lines in the manifest
     let lines = data.split(/\r?\n/);  //separates all lines into an array in the manifest
-    ArraySourceLen = lines.length; // make the length of lines the length of arrays
 
     //goes through each line in the manifest
     //if the file starts with a P that infers that the line is a check sum
     //meaning its a valid file and not a date or command
-    for(let i = 0; i < ArraySourceLen; i++){
+    for(let i = 0; i < lines.length; i++){
         let line = lines[i]; //gets a single line from the array of lines
-        FileSum = []; //this will be an array holding both the checksum(artifact id) and the file directory
+        let FileSum = []; //this will be an array holding both the checksum(artifact id) and the file directory
 
         //checks if its a checksum 
         if(line[0] == "P"){
             let checkSum_FilePath = line.split("="); //makes an array separating the check sum and the path
             FileSum.push(checkSum_FilePath[1])//add the path first
             FileSum.push(checkSum_FilePath[0])//add the check sum (artifact ID) second
+
+            //now get the manifest path and add it to the Filesum array
+            let manifestPath = repoDir + "\\.Temp\\" + FileSum[1];
+            FileSum.push(manifestPath);
+
+            //now add the path and check sum to the entire Folder
+            manifestFilesR.push(FileSum);
+
+            ArraySourceLen += 1;
         }
-
-        //now get the manifest path and add it to the Filesum array
-        manifestPath = manifestDir + "\\" + FileSum[1];
-        FileSum.push(manifestPath);
-
-        //now add the path and check sum to the entire Folder
-        manifestDir.push(FileSum)
     }  
 
     //returns all file paths along with their check sums
@@ -274,4 +276,4 @@ function getfilesManifest(repoDir, manifestDir){
 
 
 
-MergeOut('C:\\Users\\steve\\Desktop\\Target','C:\\Users\\steve\\Desktop\\Source', 'C:\\Users\\steve\\Desktop\\Target\\.Temp\\.man1.rc', "commands");
+MergeOut('C:\\Users\\steve\\Desktop\\Target','C:\\Users\\steve\\Desktop\\Source', '.man1.rc', "commands");
